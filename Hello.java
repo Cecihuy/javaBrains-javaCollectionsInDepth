feb5c8e2692f9907848c822cfc1eec68612e2e1e
sourceTree <sourcetree@mail.com>
2024-01-03 09:01:41 -0700

    28 TreeSet exercise

--- Hello.java
+++ Hello.java
@@ -1,32 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.NoSuchElementException;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
-class IteratorExercise{
-    public Collection<String> getFruits(){
-        ArrayList<String> list = new ArrayList<>();
-        list.add("apple");
-        list.add("banana");
-        list.add("cherry");
-        return list;
-    }
-}
 class Hello{
     public static void main(String[] args) {
-        IteratorExercise fruitSupply = new IteratorExercise();
-        Collection<String> fruits = fruitSupply.getFruits();
-        Iterator<String> iterator = fruits.iterator();
-        
-        while(iterator.hasNext()){
-            String fruitStr = iterator.next();
-            try{
-                if(iterator.next().equals("banana"))                
-                    iterator.remove();
-            }catch(NoSuchElementException e){
-                System.out.println("skipCuy");
-            }
-            System.out.println("fruitStr = " + fruitStr);
-        }
+        SortedSet<String> vocab = new TreeSet<>();
+        vocab.add("mango");
+        vocab.add("banana");
+        vocab.add("pineapple");
+        vocab.add("apple");
+
+        String firstWord = vocab.first();
+        String lastWord = vocab.last();
+
+        System.out.println("vocab = " + vocab);
+        System.out.println("firstWord = " + firstWord);
+        System.out.println("lastWord = " + lastWord);
+
+        System.out.println(vocab.headSet("mango"));
+        System.out.println(vocab.tailSet("mango"));
     }
 }
