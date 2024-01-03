65951795f0d0b2b3baa18f6e3b0c47a119572d9e
sourceTree <sourcetree@mail.com>
2024-01-03 02:37:46 -0700

    10 Comparator and Comparable

--- Hello.java
+++ Hello.java
@@ -1,67 +1,79 @@
-import java.util.Date;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
-class Person{
-    private String firstName;
-    private String lastName;
+class Person implements Comparable<Person>{
+    private String name;
     private int age;
-    private Date lastModifiedDate;
 
-    public Person(String firstName, String lastName, int age, Date lastModifiedDate) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+    public Person(String name, int age) {
+        this.name = name;
         this.age = age;
-        this.lastModifiedDate = lastModifiedDate;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getName() {
+        return name;
     }
     public int getAge() {
         return age;
     }
-    public void setAge(int age) {
-        this.age = age;
+
+    @Override
+    public String toString() {
+        return "Person [name=" + name + ", age=" + age + "]";
     }
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
+
+    @Override
+    public int compareTo(Person other) {
+        return Integer.compare(this.age, other.age);
     }
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-    /* write equals */
-    public boolean equals(Object o){
-        if(o == this){
-            return true;
-        }
-        if(o == null || this.getClass() != o.getClass()){
-            return false;
-        }
-        Person other = (Person) o;
-        return 
-            this.age == other.age
-            && Objects.equals(this.firstName, other.firstName)
-            && Objects.equals(this.lastName, other.lastName);
-    }
-    /* write hashCode */
-    public int hashCode(){
-        return Objects.hash(firstName, lastName, age);
-    }
+        
+    // /* write equals */
+    // public boolean equals(Object o){
+    //     if(o == this){
+    //         return true;
+    //     }
+    //     if(o == null || this.getClass() != o.getClass()){
+    //         return false;
+    //     }
+    //     Person other = (Person) o;
+    //     return 
+    //         this.age == other.age
+    //         && Objects.equals(this.firstName, other.firstName)
+    //         && Objects.equals(this.lastName, other.lastName);
+    // }
+    // /* write hashCode */
+    // public int hashCode(){
+    //     return Objects.hash(firstName, lastName, age);
+    // }
+}
+class PersonComparator implements Comparator<Person>{
+    @Override
+    public int compare(Person p1, Person p2) {
+        return p1.getName().compareTo(p2.getName());
+    }    
 }
 class Hello{
     public static void main(String[] args) {
-        Person p1 = new Person("Foo", "Bar", 25, new Date());
-        Person p2 = new Person("Foo", "Bar", 25, new Date());
-        System.out.println(p1.equals(p2));
-        System.out.println(p1.hashCode() == p2.hashCode());
+        List<Person> people = new ArrayList<>();
+        people.add(new Person("Foo", 45));
+        people.add(new Person("Bar", 31));
+        people.add(new Person("Baz", 22));
+
+        System.out.println("sort using Comparable");
+        Collections.sort(people);
+
+        for(Person efl:people){
+            System.out.println(efl);
+        }
+
+        System.out.println("sort using Comparator");
+        PersonComparator nameComparator = new PersonComparator();
+        Collections.sort(people, nameComparator);
+
+        for(Person efl:people){
+            System.out.println(efl);
+        }
     }
 }
